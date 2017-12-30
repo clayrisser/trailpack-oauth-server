008cd537701ac39e95be02fbf2e1b3159ccdbfe4
Jam Risser <jam@jamrizzi.com>
2017-12-30 13:38:37 -0600

    Fixed deps

--- api/models/waterline/Client.js
+++ api/models/waterline/Client.js
@@ -29,7 +29,7 @@ module.exports = {
         model: 'User',
         required: true,
         via: 'clients'
-      }
+      },
       authorizationCodes: {
         collection: 'AuthorizationCode',
         via: 'client'
